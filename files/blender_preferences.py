6d8d1725c6d70f49dd0e8142aa05ba301eb7794a
PhilNewm <Philippkd8@gmail.com>
2024-11-03 21:07:41 +0100

    Implement settings

--- files/blender_preferences.py
+++ files/blender_preferences.py
@@ -1,14 +1,17 @@
 import bpy
 
-preferences = bpy.context.preferences
+bpy.context.preferences.view.show_splash = False
+# bpy.context.preferences.view.show_developer_ui = False
+# bpy.context.preferences.view.show_tooltips_python = False
+# bpy.context.preferences.view.ui_line_width = "AUTO"
+# bpy.context.preferences.edit.use_negative_frames = True
+# bpy.context.preferences.themes['Default'].view_3d.vertex_size = 5
+# bpy.context.preferences.themes['Default'].view_3d.grid = Color((0.365, 0.447, 0.498, 0.106))
+# bpy.context.preferences.themes['Default'].view_3d.space.gradients.background_type = "RADIAL"
+# bpy.context.preferences.themes['Default'].view_3d.space.gradients.high_gradient = Color((0.157, 0.055, 0.231))
+# bpy.context.preferences.themes['Default'].view_3d.space.gradients.gradient = Color((0.110, 0.110, 0.1))
+# bpy.context.preferences.edit.undo_steps = 512
+# bpy.context.preferences.filepaths.save_version = 10
 
-preferences.view.show_splash = False
-preferences.view.developer_ui = False
-preferences.view.show_tooltips_python = False
-preferences.view.ui_line_width = "AUTO"
-preferences.edit.use_negative_frames = True
-preferences.themes['Default'].view_3d.vertex_size = 5
-preferences.themes['Default'].view_3d.grid = (0.365, 0.447, 0.498, 0.106)
-preferences.themes['Default'].view_3d.space.gradients.background_type = "RADIAL"
-preferences.themes['Default'].view_3d.space.gradients.high_gradient = (0.157, 0.055, 0.231)
-preferences.themes['Default'].view_3d.space.gradients.gradient = (0.031, 0.035, 0.149)
+# bpy.data.scenes["Scene"].tool_settings.lock_object_mode = False
+# bpy.ops.preferences.addon_enable(module="node_wrangler")
